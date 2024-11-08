dc68a8268e06e78ac3569324d3ec4fa369da3103
Asguho <78410939+Asguho@users.noreply.github.com>
2024-11-08 09:58:26 +0100

    init

---
+++ main.ts
@@ -0,0 +1,22 @@
+import { route, type Route } from "jsr:@std/http/unstable-route";
+import { serveDir } from "jsr:@std/http/file-server";
+import { Database } from "jsr:@db/sqlite";
+
+const db = new Database(":memory:");
+db.exec("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null,age INTEGER not null)");
+db.exec("INSERT INTO people (name, age) VALUES (?,?)", "bob", 24);
+const routes: Route[] = [
+  {
+    pattern: new URLPattern({ pathname: "/api" }),
+    handler: (req: Request) => new Response(JSON.stringify(db.prepare("select * from people").all())),
+  },
+];
+
+function defaultHandler(req: Request) {
+   return serveDir(req, {
+    fsRoot: "public",
+    showIndex: true
+  });
+}
+
+Deno.serve(route(routes, defaultHandler));
