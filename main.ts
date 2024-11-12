44c4c687e491000ffcb2b9e713b0607476635cd3
Asguho <78410939+Asguho@users.noreply.github.com>
2024-11-12 12:43:14 +0100

    update

--- main.ts
+++ main.ts
@@ -1,22 +1,43 @@
-import { route, type Route } from "jsr:@std/http/unstable-route";
-import { serveDir } from "jsr:@std/http/file-server";
-import { Database } from "jsr:@db/sqlite";
+import { serveDir } from 'jsr:@std/http/file-server';
+import { Database } from 'jsr:@db/sqlite';
 
-const db = new Database(":memory:");
-db.exec("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null,age INTEGER not null)");
-db.exec("INSERT INTO people (name, age) VALUES (?,?)", "bob", 24);
-const routes: Route[] = [
-  {
-    pattern: new URLPattern({ pathname: "/api/people" }),
-    handler: (req: Request) => new Response(JSON.stringify(db.prepare("select * from people").all())),
-  },
-];
+const db = new Database(':memory:');
 
-function defaultHandler(req: Request) {
-   return serveDir(req, {
-    fsRoot: "public",
-    showIndex: true
+db.exec(
+  'CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT not null, sirName TEXT not null,age INTEGER not null)'
+);
+
+Deno.serve(async (req) => {
+  const pathname = new URL(req.url).pathname;
+
+  if (pathname == '/api/people') {
+    return new Response(JSON.stringify(db.prepare('select * from people').all()));
+  }
+
+  if (pathname == '/api/addPeople') {
+    const formdata = await req.formData();
+
+    if (!(formdata.get('name') && formdata.get('sirName') && formdata.get('age'))) {
+      return new Response('Please fill all the fields');
+    }
+
+    db.exec(
+      'INSERT INTO people (name, sirName, age) VALUES (?,?, ?)',
+      formdata.get('name')?.toString(),
+      formdata.get('sirName')?.toString(),
+      formdata.get('age')?.toString()
+    );
+
+    return new Response(null, {
+      status: 303,
+      headers: {
+        Location: '/',
+      },
+    });
+  }
+
+  return serveDir(req, {
+    fsRoot: 'public',
+    showIndex: true,
   });
-}
-
-Deno.serve(route(routes, defaultHandler));
+});
